6adfd72b7526b43cc3ca0f46862e93b59f6a7792
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-14 21:05:34 +0300

    Created Index Page & Controller. Closes #7
    Took 16 minutes

---
+++ pet-clinic-web/src/main/java/vlad/springframework/vladpetclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package vlad.springframework.vladpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"/", "", "index", "index.html"})
+    public String index() {
+        return "index"; // wires up controller for templates/index.html
+    }
+
+}
