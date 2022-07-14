857f81c29dd54ca96dc821b6d6dc1813a6f0e931
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-14 21:50:14 +0300

    Refactored VetController
    Took 3 minutes

--- pet-clinic-web/src/main/java/vlad/springframework/vladpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/vlad/springframework/vladpetclinic/controllers/VetController.java
@@ -3,9 +3,10 @@ package vlad.springframework.vladpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html"})
     public String listVets() {
         return "vets/index";
     }
