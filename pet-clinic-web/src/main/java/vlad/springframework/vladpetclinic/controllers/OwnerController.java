fd5bac51eed9e9c72ff0a9b47e67bb76aaf2d15b
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-14 21:47:23 +0300

    Create Owner Index Page and Controller. Closes #12
    Took 7 minutes

---
+++ pet-clinic-web/src/main/java/vlad/springframework/vladpetclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package vlad.springframework.vladpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(){
+        return "owners/index";
+    }
+
+}
