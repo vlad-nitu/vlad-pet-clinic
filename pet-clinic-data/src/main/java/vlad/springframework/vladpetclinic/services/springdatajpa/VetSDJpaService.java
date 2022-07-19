12004e2bd344817a701d9247cacd6244fb48703e
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-19 19:22:29 +0300

    Created Spring Data JPA Service Impl for Vet. Closes #41
    Took 11 minutes
    
    Took 9 minutes

---
+++ pet-clinic-data/src/main/java/vlad/springframework/vladpetclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,43 @@
+package vlad.springframework.vladpetclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import vlad.springframework.vladpetclinic.model.Owner;
+import vlad.springframework.vladpetclinic.model.Vet;
+import vlad.springframework.vladpetclinic.repositories.VetRepository;
+import vlad.springframework.vladpetclinic.services.VetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vet -> vets.add(vet));
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+}
