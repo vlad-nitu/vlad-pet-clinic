474ea5f07332c7640f9126613af5ce2049ecd70c
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-19 16:53:06 +0300

    Implemented Base Entity using @MappedSuperclass. Closes #35
    Took 6 minutes

--- pet-clinic-data/src/main/java/vlad/springframework/vladpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/vlad/springframework/vladpetclinic/model/BaseEntity.java
@@ -1,11 +1,17 @@
 package vlad.springframework.vladpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
     private Long id;
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long getId() {
         return id;
     }
