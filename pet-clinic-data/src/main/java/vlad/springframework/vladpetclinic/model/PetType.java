1e6a67826aa60a2f73385a91e07a62fe36ffca9b
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-19 17:21:00 +0300

    Convert Owner to JPA Entities. Closes #36
    Took 24 minutes

--- pet-clinic-data/src/main/java/vlad/springframework/vladpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/vlad/springframework/vladpetclinic/model/PetType.java
@@ -1,6 +1,14 @@
 package vlad.springframework.vladpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
+    @Column(name = "name")
+    private String name;
     public String getName() {
         return name;
     }
@@ -8,6 +16,4 @@ public class PetType extends BaseEntity{
     public void setName(String name) {
         this.name = name;
     }
-
-    private String name;
 }
