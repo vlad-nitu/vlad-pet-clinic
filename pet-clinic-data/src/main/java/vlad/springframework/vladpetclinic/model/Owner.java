e00ef0ce96a03d957845086934d76196a6f89780
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-20 14:38:06 +0300

    Added Project Lombok & Refactored. Added #51
    Took 19 minutes

--- pet-clinic-data/src/main/java/vlad/springframework/vladpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/vlad/springframework/vladpetclinic/model/Owner.java
@@ -1,9 +1,14 @@
 package vlad.springframework.vladpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
@@ -18,35 +23,12 @@ public class Owner extends Person {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
+    @Builder
+    public Owner(Long id, String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
+        super(id, firstName, lastName);
         this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
         this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
         this.telephone = telephone;
-    }
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
         this.pets = pets;
     }
 }
--- pet-clinic-data/src/main/java/vlad/springframework/vladpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/vlad/springframework/vladpetclinic/model/Vet.java
@@ -1,9 +1,16 @@
 package vlad.springframework.vladpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person{
@@ -14,11 +21,4 @@ public class Vet extends Person{
     )
     private Set<Speciality> specialities = new HashSet<>();
 
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
 }
