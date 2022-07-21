646ef048f28956f94e69de73367791557c5746cb
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-21 14:07:51 +0300

    Tested that Spring Context loads
    Took 15 minutes

---
+++ pet-clinic-web/src/test/java/vlad/springframework/vladpetclinic/VladPetClinicApplicationTest.java
@@ -0,0 +1,17 @@
+package vlad.springframework.vladpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class VladPetClinicApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
