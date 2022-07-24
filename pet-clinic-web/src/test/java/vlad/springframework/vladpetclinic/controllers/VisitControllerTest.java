08a7b93fcdeac42893cb94e608d6eb0240c5657a
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-24 18:14:50 +0300

    Added MockMVC tests for VisitController. Closes #67
    Took 25 minutes

---
+++ pet-clinic-web/src/test/java/vlad/springframework/vladpetclinic/controllers/VisitControllerTest.java
@@ -0,0 +1,71 @@
+package vlad.springframework.vladpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.util.UriTemplate;
+import vlad.springframework.vladpetclinic.model.Pet;
+import vlad.springframework.vladpetclinic.model.Visit;
+import vlad.springframework.vladpetclinic.services.PetService;
+import vlad.springframework.vladpetclinic.services.VisitService;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class VisitControllerTest {
+    private static final String PETS_CREATE_OR_UPDATE_VISIT_FORM = "pets/createOrUpdateVisitForm";
+    private final String visitsUri = new String("/owners/1/pets/1/visits/new");
+    @Mock
+    private VisitService visitService;
+    @Mock
+    private PetService petService;
+    private VisitController visitController;
+    private MockMvc mockMvc;
+
+
+    @BeforeEach
+    void setUp() {
+        visitController = new VisitController(visitService, petService);
+        mockMvc = MockMvcBuilders.standaloneSetup(visitController).build();
+        when(petService.findById(anyLong()))
+                .thenReturn(Pet.builder().id(1L).visits(new HashSet<>()).build());
+    }
+
+
+    @Test
+    void initNewVisitForm() throws Exception {
+        mockMvc.perform(get(visitsUri))
+                .andExpect(status().isOk())
+                .andExpect(view().name(PETS_CREATE_OR_UPDATE_VISIT_FORM));
+    }
+    @Test
+    void processNewVisitForm() throws Exception{
+        mockMvc.perform(post(visitsUri)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("date", any(String.class))
+                .param("description", "some description"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/{ownerId}"));
+        verify(visitService).save(any(Visit.class));
+
+
+    }
+}
