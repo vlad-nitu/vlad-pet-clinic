7c74d193cac96ff5bfb6f107ec13d713b5230287
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-25 15:54:38 +0300

    Impl Vets JSON endpoint. Closes #69
    Took 15 minutes

--- pet-clinic-web/src/main/java/vlad/springframework/vladpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/vlad/springframework/vladpetclinic/controllers/VetController.java
@@ -2,8 +2,13 @@ package vlad.springframework.vladpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import vlad.springframework.vladpetclinic.model.Vet;
 import vlad.springframework.vladpetclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -19,5 +24,10 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+    @GetMapping("/api/vets")
+    @ResponseBody
+    public Set<Vet> getVetsJSON() {
+        return vetService.findAll();
     }
 }
