4bc66c6fdbea91917be440994dbbffdb7152a10f
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-25 15:00:59 +0300

    Pet birthDate @DateTimeFormat binding added. Closes #81
    Took 8 minutes

--- pet-clinic-data/src/main/java/vlad/springframework/vladpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/vlad/springframework/vladpetclinic/model/Pet.java
@@ -1,6 +1,7 @@
 package vlad.springframework.vladpetclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -37,6 +38,7 @@ public class Pet extends BaseEntity {
     @JoinColumn(name = "owner_id")
     private Owner owner;
     @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
